11b0d5217db4bb4c1a24d7c10b32e2001971a429
최지철 <sksk02zja@gmail.com>
2025-04-13 19:02:47 +0900

    [Feat] WSLabel 텍스트 있을시, 자동으로 Size 맞추게하기

---
+++ 24th-App-Team-1-iOS/Feature/MessageFeature/Sources/Presentation/ViewControllers/BottomSheet/AnonymousProfileBottomSheetsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AnonymousProfileBottomSheetsViewController.swift
+//  MessageFeature
+//
+//  Created by 최지철 on 4/13/25.
+//
+
+import UIKit
+
+class AnonymousProfileBottomSheetsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
